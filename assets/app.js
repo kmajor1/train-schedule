d8e5d66856838db6807788a85eec406bf5f35041
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-10-31 19:30:28 -0400

    moved initialization code into app.js

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+    apiKey: "AIzaSyDkKRcvX2zVoxzwZHHFpiDPxh5s6JWiGx8",
+    authDomain: "train-schedule-d207a.firebaseapp.com",
+    databaseURL: "https://train-schedule-d207a.firebaseio.com",
+    projectId: "train-schedule-d207a",
+    storageBucket: "train-schedule-d207a.appspot.com",
+    messagingSenderId: "608051141696"
+};
+firebase.initializeApp(config);

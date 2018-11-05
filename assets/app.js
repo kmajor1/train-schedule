876e9a0206020c731114da90fa6d46441bc5573c
kmajor1 <42553285+kmajor1@users.noreply.github.com>
2018-11-05 18:29:14 -0500

    added moment js library. Now storing unix and regular times in Firebase

--- assets/app.js
+++ assets/app.js
@@ -10,20 +10,33 @@ var config = {
 // initialize firebase app 
 firebase.initializeApp(config);
 
+// global train time variable 
+var trainTimeStamp;
+
 // load database object 
 var database = firebase.database();
 // load train schedule ref on train db 
 var trainListRef = database.ref();
 // object reference for pushing data to train list reference 
-var addTrain = function (trainName, trainDest, trainFirstTime, trainFreq) {
+var addTrain = function (trainName, trainDest, trainFirstTime, trainFreq, unixStartTime) {
+    console.log("values passed were: ");
+    console.log(trainName + " " + trainDest + " "+ trainFirstTime + " " + trainFreq + " " + unixStartTime); 
     trainListRef.push({
         name: trainName,
         destination: trainDest,
         startTime: trainFirstTime,
-        frequency: trainFreq
+        frequency: trainFreq,
+        startTimeStamp: unixStartTime
     })
 }
 
+// test add train 
+var trainName = "some name"; 
+var trainDest = "somewhere";
+var trainFirstTime = moment().hours('15').minutes('15').format();
+var trainFreq = '12';
+var unixStartTime = moment().hours('15').minutes('15').format('x');
+addTrain(trainName, trainDest, trainFirstTime, trainFreq, unixStartTime); 
 // onclick event handler for "add train" button 
 
 document.getElementById("submitNewTrain").onsubmit = function (event) {
@@ -32,13 +45,16 @@ document.getElementById("submitNewTrain").onsubmit = function (event) {
     var trainDestInput = document.getElementById("inputTrainDestination").value; 
     var trainStartInput = document.getElementById("inputFirstTrainTime").value; 
     var trainFreqInput = document.getElementById("inputTrainFrequency").value;
+    var hours = trainStartInput.substring(0,2);
+    var minutes  = trainStartInput.substring(3,5);
+    // convert to moment time 
+    var trainStart = moment().hour(hours).minutes(minutes).seconds('00').format(); 
+    // store in unix timestamp 
+    var trainStartUnix = moment().hour(hours).minutes(minutes).seconds('00').format('x');
+    
     // call function to push to db 
-    addTrain(trainNameInput, trainDestInput, trainStartInput, trainFreqInput);
+    addTrain(trainNameInput, trainDestInput, trainStart, trainFreqInput, trainStartUnix);
 }
-
-
-
-
 
 // child_added handler
 trainListRef.on('child_added', function (snapshot) {
